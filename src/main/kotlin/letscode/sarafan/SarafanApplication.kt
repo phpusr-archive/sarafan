3338f90a29ed8ae866e3977c82a962a39803dfe0
phpusr <phpusr@gmail.com>
2018-09-19 18:33:53 +0500

    Init repository

---
+++ src/main/kotlin/letscode/sarafan/SarafanApplication.kt
@@ -0,0 +1,11 @@
+package letscode.sarafan
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SarafanApplication
+
+fun main(args: Array<String>) {
+    runApplication<SarafanApplication>(*args)
+}
